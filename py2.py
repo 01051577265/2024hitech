0218c477ceea5d39e8d5e5ecf7aa9e94d76b42a4
jo min hee <162547786+01051577265@users.noreply.github.com>
2024-03-07 16:14:17 +0900

    Add files via upload

---
+++ py2.py
@@ -0,0 +1,23 @@
+#비교연산자
+
+a = 1
+if a <= 2:
+    print("a는 2보다 작거나 같다")
+
+
+
+#할당연산자
+a= a/10
+a/=10
+
+print(a)
+
+
+#논리연산자
+x=True
+y=False
+
+if x or y
+    print("yes")
+else:
+    print("no")
---
+++ py3.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#20240307
+#파이썬 기본데이터 타입
+
+
+# In[2]:
+
+
+print("hello, world!")
+
+
+# In[3]:
+
+
+a=1
+b=2
+c=a+b
+
+print(c)
+
+
+# In[4]:
+
+
+#3 - 정수형
+import math
+
+int(3.5)
+
+
+# In[5]:
+
+
+korea="동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세"
+print(korea)
+print(korea)
+print(korea)
+
+
+# In[6]:
+
+
+#산술연산자
+a=4
+b=6
+
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+
+print(c)
+print(d)
+print(e)
+print(f)
+
+
+a=[1,2,3,4]
+b=3 in a  #True
+print(b)
+
