45bebe019745b53ada5f3d58b33e204c45a0acf3
jo min hee <162547786+Jominhee06@users.noreply.github.com>
2024-03-11 13:45:18 +0900

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,23 @@
+#%%
+print('hello python world')
+#%%
+a = 2
+a = 'str123'
+a = 123.123
+#%%
+print(a)
+a = 1
+print(a)
+a = '123'
+print(a)
+a = 123.123
+print(a)
+# %%
+a = 50
+b = 20
+c = 52
+e = a+b+c
+f = a*b*c
+g = a/b/c
+print(e,f,g)
+# %%
